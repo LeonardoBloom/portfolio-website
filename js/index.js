35ef4fa0890f77edf7f9d853b6c2dd9077bdcf9e
LeonardoBloom <135758876+LeonardoBloom@users.noreply.github.com>
2024-01-19 03:25:08 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -13,6 +13,36 @@ const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
 
+// ARROW ANIMATION SCROLLS TO ABOUT
+const arrow = document.getElementById('arrowAnim');
+var aboutScroll = document.querySelector('.info');
+
+var Arrowtarget = aboutScroll.offsetTop - 50;
+
+arrow.addEventListener('click', () => {
+    window.scrollTo({
+        top: Arrowtarget,
+        behavior: 'smooth',
+    });
+});
+// END: ARROW SCROLL
+
+// PROJECTS SCROLL
+const projectsButton = document.getElementById('work-button');
+const projects = document.querySelector('.projects');
+
+var ProjTarget = projects.offsetTop - 55;
+
+projectsButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: ProjTarget,
+        behavior: 'smooth',
+    });
+});
+
+// END PROJECTS SCROLL
+
+
 /** This code is unused */
 const observerUp = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
